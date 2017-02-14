8234dc3a2bb2057fcf25dd324ac5fc1a94e9503a
sstpierre291 <sstpierre291@gmail.com>
2017-02-14 00:18:52 -0500

    exercise05

---
+++ exercises/exercise05/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+  $("#open").click(function() {
+    responsiveVoice.speak("No,Fuck you!","UK English Female");
+  });
+
+  $("#next").click(function() {
+    responsiveVoice.speak("Go to hell!","UK English Female");
+  });
+
+  $("#last").click(function() {
+    responsiveVoice.speak("You won't tell me what to do!","UK English Female");
+  });
+
+  $("#home").click(function() {
+    responsiveVoice.speak("Screw you!","UK English Female");
+  });
+
+  $("#close").click(function() {
+    responsiveVoice.speak("You're stuck in here... looser!","UK English Female");
+  });
+
+
+
+
+
+});
